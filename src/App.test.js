3bc274e3101d574d60ed32e00934db22bebd7780
Westley Strellis <wstrellis@gmail.com>
2020-06-25 14:29:39 -0400

    updated failing test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const greeting = getByText(/westley's blob page/i);
+  expect(greeting).toBeInTheDocument();
 });
