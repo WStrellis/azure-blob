aa2ded1a7b21f6eb40d035fbe11a242f7994d875
Westley Strellis <wstrellis@gmail.com>
2020-06-25 20:30:19 -0400

    broke the app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Westley's Blob Page
+          broken
         </p>
       </header>
     </div>
